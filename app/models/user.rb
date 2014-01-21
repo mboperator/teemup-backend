701fb08d412374c34e122bcf9231584980b08fec
mikeastock <mikeastock@gmail.com>
2014-01-20 20:15:30 -0800

    Add user validation for uniqness and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 
   has_many :api_keys
 
+  validates :email, presence: true, uniqueness: true, format: /\A*.+@.+\z/
+  validates :password, presence: true, length: { minimum: 6 }, if: :password
+
   before_save { self.email = email.downcase }
   after_save :create_access_token
 
