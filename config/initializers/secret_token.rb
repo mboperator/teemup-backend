951068f282d1d7376e6fccc5c7dff080f9082ff3
mikeastock <mikeastock@gmail.com>
2014-01-20 18:44:40 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teemup::Application.config.secret_key_base = 'b21c943991c6395653ac28e4b2a798b1171a575109d232374a75f5cbf5412cf4ef28adcc6f45c7bcc895863527a3b55bb43d6c86fd14724753668dba7a617e8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Teemup::Application.config.session_store :cookie_store, key: '_teemup_session'
