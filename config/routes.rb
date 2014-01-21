74c81b8bec7883fd7890b9e121ecbf11ee135330
Marcus Bernales <mboperator@gmail.com>
2014-01-21 11:45:58 -0800

    Created events, and groups controllers Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Teemup::Application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:create, :update, :show]
+      resources :groups, only: [:create, :update, :show, :destroy]
     end
   end
 end
