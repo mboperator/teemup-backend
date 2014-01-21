e51115d378472db2b1998332a8cd1d5780685273
Marcus Bernales <mboperator@gmail.com>
2014-01-20 20:42:21 -0800

    Created events, and groups controllers Created routes

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,71 +1,73 @@
+module Api
+  module v1
+    class EventsController < ApiController
+      responds_to :json
 
-class EventsController < ApplicationController
-  responds_to :json
+      # GET /group/:group_id/events.json
+      def index
+        @group = Group.find_by(id: params[:group_id])
+        @events = @group.events
 
-  # GET /group/:group_id/events.json
-  def index
-    @group = Group.find_by(id: params[:group_id])
-    @events = @group.events
-
-    respond_to do |format|
-      format.html # index.html.haml
-      format.json { render json: @events }
-    end
-  end
-
-  # GET /group/:group_id/events/:id.json
-  def show
-    @event = Event.find_by(id: params[:id])
-
-    respond_to do |format|
-      format.html # show.html.haml
-      format.json { render json: @event }
-    end
-  end
-
-  # POST /group/:group_id/events.json
-  def create
-    @event = Event.new(event_params)
-    @event.creator = current_user
-    @event.group = Group.find_by(params[:group_id])
-
-
-    respond_to do |format|
-      if @event.save
-        format.json { render json: @event, status: :created, location: @event }
-      else
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html # index.html.haml
+          format.json { render json: @events }
+        end
       end
-    end
-  end
 
-  # PUT /events/1
-  # PUT /events/1.json
-  def update
-    @event = Event.new(params[:event])
+      # GET /group/:group_id/events/:id.json
+      def show
+        @event = Event.find_by(id: params[:id])
 
-    respond_to do |format|
-      if @event.update_attributes(event_params)
-        format.json { head :no_content }
-      else
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          format.html # show.html.haml
+          format.json { render json: @event }
+        end
       end
+
+      # POST /group/:group_id/events.json
+      def create
+        @event = Event.new(event_params)
+        @event.creator = current_user
+        @event.group = Group.find_by(params[:group_id])
+
+
+        respond_to do |format|
+          if @event.save
+            format.json { render json: @event, status: :created, location: @event }
+          else
+            format.json { render json: @event.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # PUT /group/:group_id/events/:id.json
+      def update
+        @event = Event.new(params[:event])
+
+        respond_to do |format|
+          if @event.update_attributes(event_params)
+            format.json { head :no_content }
+          else
+            format.json { render json: @event.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /group/:group_id/events/:id.json
+      def destroy
+        @event = Event.find_by(params[:id])
+        @event.destroy
+
+        respond_to do |format|
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      def event_params
+        params.require(:event).permit(:name, :subtitle, :creator, :location => { :lat, :lon })
+      end
+
     end
   end
-
-  # DELETE /events/1
-  # DELETE /events/1.json
-  def destroy
-    @event = Event.find_by(params[:id])
-    @event.destroy
-
-    respond_to do |format|
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    def event_params
-      params.require(:event).permit(:name, :subtitle, :creator, :location => { :lat, :lon })
-    end
 end
--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,76 +1,79 @@
+module Api
+  module v1
+    class GroupsController < ApiController
+      responds_to :json
 
-class GroupsController < ApplicationController
-  responds_to :json
+      # GET /groups
+      # GET /groups.json
+      def index
+        @user = current_user
+        @groups = current_user.groups
 
-  # GET /groups
-  # GET /groups.json
-  def index
-    @user = current_user
-    @groups = current_user.groups
-
-    respond_to do |format|
-      format.html # index.html.haml
-      format.json { render json: @groups }
-    end
-  end
-
-  # GET /groups/1
-  # GET /groups/1.json
-  def show
-    @group = Group.find_by(id: params[:id])
-    if current_user.groups.include?(@group)
-      respond_to do |format|
-        format.html # show.html.haml
-        format.json { render json: @group }
+        respond_to do |format|
+          format.html # index.html.haml
+          format.json { render json: @groups }
+        end
       end
-    else
 
-    end
-  end
+      # GET /groups/1
+      # GET /groups/1.json
+      def show
+        @group = Group.find_by(id: params[:id])
+        if current_user.groups.include?(@group)
+          respond_to do |format|
+            format.html # show.html.haml
+            format.json { render json: @group }
+          end
+        else
 
-  # POST /groups
-  # POST /groups.json
-  def create
-    @group = Group.new(group_params)
-    @group.creator = current_user
+        end
+      end
+
+      # POST /groups
+      # POST /groups.json
+      def create
+        @group = Group.new(group_params)
+        @group.creator = current_user
 
 
-    respond_to do |format|
-      if @group.save
-        format.json { render json: @group, status: :created, location: @group }
-      else
-        format.json { render json: @group.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          if @group.save
+            format.json { render json: @group, status: :created, location: @group }
+          else
+            format.json { render json: @group.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # PUT /groups/1
+      # PUT /groups/1.json
+      def update
+        @group = Group.new(params[:group])
+
+        respond_to do |format|
+          if @group.update_attributes(group_params)
+            format.json { head :no_content }
+          else
+            format.json { render json: @group.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /groups/1
+      # DELETE /groups/1.json
+      def destroy
+        @group = Group.find_by(params[:id])
+        @group.destroy
+
+        respond_to do |format|
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      def group_params
+        params.require(:group).permit(:name, :creator)
       end
     end
   end
-
-  # PUT /groups/1
-  # PUT /groups/1.json
-  def update
-    @group = Group.new(params[:group])
-
-    respond_to do |format|
-      if @group.update_attributes(group_params)
-        format.json { head :no_content }
-      else
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /groups/1
-  # DELETE /groups/1.json
-  def destroy
-    @group = Group.find_by(params[:id])
-    @group.destroy
-
-    respond_to do |format|
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    def group_params
-      params.require(:group).permit(:name, :creator)
-    end
 end
