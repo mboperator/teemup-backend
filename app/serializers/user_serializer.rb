b29b75e5aa2c5581ddb045e81409bea8cee1c669
Michael Stock <mikeastock@gmail.com>
2014-01-28 22:50:38 -0800

    Change user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :email, :phone_number
-  
-  def full_name
-    "#{object.first_name} #{object.last_name}"
-  end
+  attributes :id, :first_name, :last_name, :email, :phone_number
 end
