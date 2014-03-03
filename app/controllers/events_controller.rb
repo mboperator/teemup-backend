4afd9a750a71e56644691992588fed43a1f9374c
Marcus Bernales <mboperator@gmail.com>
2014-03-02 18:46:58 -0800

    Duration now entered in hours instead of seconds.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,13 +15,14 @@ class EventsController < ApplicationController
                       .merge(created_by_id: current_user.id)
                       .merge(location_id: loc.id)
                       .merge(group_id: current_group.id))
+    @event.duration = @event.duration * 3600
     if @event.save
       redirect_to group_url(@group.id), notice: 'Event was successfully created.'
     end
   end
 
   def new
-    @event = Event.new
+    @event = Event.new(duration: 3600)
     loc = Location.new(lat: 34.4155193, lon:-119.8511707)
     @event.location = loc
   end
@@ -35,6 +36,8 @@ class EventsController < ApplicationController
     loc = Location.find_or_create_by(location_params) if location_params
     if @event.update_attributes(event_params
                                 .merge(location_id: loc.id))
+      @event.duration = @event.duration * 3600
+      @event.save
       redirect_to [@group, @event] , notice: 'Event was successfully updated.'
     end
   end
@@ -53,12 +56,12 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event)
     .permit(:name, :subtitle, :created_by_id, :group_id, :start_time, :duration, :description, tag_ids: [])
-
   end
 
   def location_params
     params.require(:location)
     .permit(:lat, :lon)
   end
+
 end
 
