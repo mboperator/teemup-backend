6b19bb79d496662426095d65769d57fa37f6eac2
Marcus Bernales <mboperator@gmail.com>
2014-03-05 00:38:41 -0800

    Fixed photo uploads Fixed null location_name Added pic_url to serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :start_time, :duration_in_minutes, :lat, :lon, :location_name, :event_tags
+  attributes :id, :name, :description, :start_time, :duration_in_minutes,
+             :lat, :lon, :location_name, :event_tags, :pic_url
 
 
   def lat
@@ -16,6 +17,14 @@ class EventSerializer < ActiveModel::Serializer
 
   def event_tags
     object.tags.pluck(:name)
+  end
+
+  def location_name
+    "#{object.location_name}"
+  end
+
+  def pic_url
+    "http://teemup.us#{object.picture.url(:medium)}"
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Teemup::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  Paperclip.options[:command_path] = "/usr/bin/"
+
 end
